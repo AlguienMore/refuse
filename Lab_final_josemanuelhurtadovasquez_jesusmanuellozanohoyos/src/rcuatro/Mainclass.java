91935f56a8bf30adb8a4ea5e2c193a55307b3874
AlguienMore <hurtadojosmanuel@gmail.com>
2017-05-11 19:55:05 -0500

    Arreglo declarado

--- Lab_final_josemanuelhurtadovasquez_jesusmanuellozanohoyos/src/rcuatro/Mainclass.java
+++ Lab_final_josemanuelhurtadovasquez_jesusmanuellozanohoyos/src/rcuatro/Mainclass.java
@@ -5,7 +5,9 @@ public class Mainclass {
 	
 	public static void main(String[] args) {
 		Scanner gabe = new Scanner(System.in);
-		int[][][] DatosVendor = new int[100][100][100];
+		int[][][] DatosVendor = new int[100][100][100]; //1ra Dimension= Código 2da dimensión=Cantidades Vendidas 3ra dimension= Vendido en pesos
+		
+		
 	}
 
 }
