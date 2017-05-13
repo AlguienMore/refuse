96b6b910f80aa2ce4201ecfdf99d886b26f225cf
AlguienMore <hurtadojosmanuel@gmail.com>
2017-05-13 18:16:03 -0500

    Updated

--- Lab_final_josemanuelhurtadovasquez_jesusmanuellozanohoyos/src/rcuatro/Mainclass.java
+++ Lab_final_josemanuelhurtadovasquez_jesusmanuellozanohoyos/src/rcuatro/Mainclass.java
@@ -4,12 +4,17 @@ import java.util.*;
 public class Mainclass {
 	static int menu = 6;
 	static Scanner sc = new Scanner(System.in);
-	String terven= null;
-	int i = 0;
+	String terven= "";
+	boolean seg = true;
+	int i = 0, sum =0, n= 0;
 	int v [][] = new int[100][100];
-	public void ventas(){
+	int vendor[] = new int[100];
+	int vendordatos[][] = new int [100][100];
+	String[] args = null;
+	public int[][] ventas(){
 		System.out.println("Datos de las Ventas.\n");
 		do{
+			seg = true;
 			System.out.print("CODIGO DEL VENDEDOR:\n>");
 			v[i][i]= sc.nextInt();
 			System.out.print("\nCODIGO DEL COMPRADOR:\n>");
@@ -20,36 +25,56 @@ public class Mainclass {
 			v[i][i+3]=sc.nextInt();
 			System.out.print("\nCANTIDAD VENDIDA:\n>");
 			v[i][i+4]=sc.nextInt();
+			i++;
+			System.out.print("\n¿DESEA REGISTRAR OTRA VENTA?:\n>");
+			while(seg){
+			terven = sc.nextLine();
+			if (terven.equalsIgnoreCase("si")||terven.equalsIgnoreCase("no")){
+				seg = false;
+			}else if(!terven.equalsIgnoreCase("")) {System.out.println("Por favor, digite una respuesta valida. \n>");}
+			}
 		}while (terven.equalsIgnoreCase("si"));
+		
+		return v;
 	}
 	
 	public void registro(){
-		System.out.print("registro");
+		System.out.println("Registro de ventas por vendedor.\n");
+		for(i = 0; i < n; i++){
+			sum += v[i][i+4];
+		}
+		System.out.print(sum);
+		Mainclass.main(args);
 	}
 	
 	public void sueldo(){
 		System.out.print("sueldo");
+		Mainclass.main(args);
 	}
 	
 	public void visual(){
 		System.out.print("visualizacion");
+		Mainclass.main(args);
 	}
 	
 	public void consultas(){
 		System.out.print("consultas");
+		Mainclass.main(args);
 	}
 	
 	public void ingreso(){
 		System.out.print("ingreso");
-		
+		Mainclass.main(args);
 	}
 		
 	public void despido(){
 		System.out.print("despido");
+		Mainclass.main(args);
 	}
 	
 	public void salir(){
 		System.out.print("salir");
+		Mainclass.main(args);
 	}
 	
 	public static void main(String[] args) {
